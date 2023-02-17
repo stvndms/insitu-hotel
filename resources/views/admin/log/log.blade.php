5c573e3743bfb1a01acee76e683c394733b183fa
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-02-17 14:37:32 +0700

    Log page

--- resources/views/admin/log/log.blade.php
+++ resources/views/admin/log/log.blade.php
@@ -35,7 +35,7 @@
                                 <thead class="align-bottom">
                                     <tr>
                                         <th class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">History</th>
-                                        <th class="px-6 py-3 pl-2 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">Date</th>
+                                        <th class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">Date</th>
                                         <th class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">Username</th>
                                         
                                     </tr>
