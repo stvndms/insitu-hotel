864fe4f9b0e7fece20b642ba167d012838d49c34
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-02-17 09:49:27 +0700

    Room migration

---
+++ database/migrations/2023_02_13_141338_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('room_number')->unique();
+            $table->foreignId('facility_id')->constrained('facilities');
+            $table->foreignId('room_type')->constrained('room_types');
+            $table->integer('room_price');
+            $table->string('room_image')->nullable();
+            $table->string('random_str')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
