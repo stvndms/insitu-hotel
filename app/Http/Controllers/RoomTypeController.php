8f93847fa6be9a7c866974cfdc9be844c7092086
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-02-17 09:05:58 +0700

    Make RoomType Pages

---
+++ app/Http/Controllers/RoomTypeController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RoomType;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class RoomTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roomTypes = RoomType::all();
+        return view('admin.roomType.roomType', compact('roomTypes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.roomType.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'room_type' => ['required'],
+            'description' => ['required']
+        ];
+        $validatedData = $request->validate($rules);
+        $validatedData['random_str'] = Str::random(30);
+        RoomType::create($validatedData);
+        return redirect(route('room-type.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RoomType  $roomType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RoomType $roomType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RoomType  $roomType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RoomType $roomType)
+    {
+        return view('admin.roomType.edit', compact('roomType'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RoomType  $roomType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RoomType $roomType)
+    {
+        $rules = [
+            'room_type' => ['required'],
+            'room_type' => ['required']
+        ];
+        $validatedData = $request->validate($rules);
+        RoomType::where('id', $roomType->id)->update($validatedData);
+        return redirect(route('room-type.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RoomType  $roomType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RoomType $roomType)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/roomType/edit.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.admin.index')
+
+@section('container')
+<main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
+    <!-- Navbar -->
+    <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all shadow-none duration-250 ease-soft-in rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="true">
+        <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
+            <nav>
+                <!-- breadcrumb -->
+                <ol class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16">
+                    <li class="leading-normal text-sm">
+                        <a class="opacity-50 text-slate-700" href="javascript:;">Pages</a>
+                    </li>
+                    <li class="text-sm pl-2 capitalize leading-normal text-slate-700 before:float-left before:pr-2 before:text-gray-600 before:content-['/']" aria-current="page">Room Types</li>
+                </ol>
+                <h6 class="mb-0 font-bold capitalize">Room Types</h6>
+            </nav>
+        </div>
+    </nav>
+
+    <!-- end Navbar -->
+
+    <div class="w-full px-6 py-2 mx-auto">
+        <!-- table 1 -->
+
+        <div class="flex -mx-3">
+            <div class="flex-none w-full max-w-full px-3">
+                <div class="relative flex flex-col min-w-0 mb-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
+                    <div class="p-6 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
+                        <h6>Edit</h6>
+                    </div>
+
+                    <div class="flex-auto p-6">
+                        <form role="form" action="{{ route('room-type.update', $roomType->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Room Type</label>
+                            <div class="mb-4">
+                                <input type="text" class="text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-orange focus:transition-shadow" placeholder="Room Type" name="room_type" value="{{ $roomType->room_type }}"/>
+                            </div>
+                            <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Description</label>
+                            <div class="mb-4">
+                                <input type="text" class="text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-orange focus:transition-shadow" placeholder="Decription" name="description" value="{{ $roomType->description }}"/>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="inline-block w-full px-6 py-3 mt-6 mb-0 font-bold text-center text-black uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer shadow-soft-md bg-x-25 bg-150 leading-pro text-xs ease-soft-in tracking-tight-soft bg-orange hover:scale-102 hover:shadow-soft-xs active:opacity-85">Update</button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</main>    
+@endsection
