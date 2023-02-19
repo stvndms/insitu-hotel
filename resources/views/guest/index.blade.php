cf483a15acb356bf4ff6ba18b6810b1c1f9ab712
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-02-19 17:41:24 +0700

    Remove boilerplate

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,4 +1,4 @@
-<h5> in tab user anjing </h5>
+<h5> in tab user</h5>
 <form action="{{ route('logout') }}" method="POST">
     @csrf
     <button class="nav-link px-3 bg-light border-0" type="submit" onclick="return('Anda yakin ingin keluar?')">Logout</button>
--- resources/views/receptionist/index.blade.php
+++ resources/views/receptionist/index.blade.php
@@ -1,4 +1,4 @@
-<h5> in tab resepsionis anjing </h5>
+<h5> in tab resepsionis</h5>
 <form action="{{ route('logout') }}" method="POST">
     @csrf
     <button class="nav-link px-3 bg-light border-0" type="submit" onclick="return('Anda yakin ingin keluar?')">Logout</button>
