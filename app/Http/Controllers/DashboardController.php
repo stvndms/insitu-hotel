129b981ad06b6a4164ffe4b728d33c834fcf5a53
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-04-14 17:11:23 +0700

    Admin page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,7 +12,7 @@ class DashboardController extends Controller
     {
         if (Auth::user()->role === 'admin') {
             $title = 'Administrator Dashboard';
-            return view('admin.userManagement.userManagement', compact('title'));
+            return view('admin.index', compact('title'));
 
         } elseif (Auth::user()->role === 'receptionist') {
             $title = 'Receptionist Dashboard';
--- resources/views/layouts/admin/index.blade.php
+++ resources/views/layouts/admin/index.blade.php
@@ -146,6 +146,7 @@
                     </a>
                 </li>
 
+                @if (auth()->user()->role == 'superAdmin')
                 <li class="mt-0.5 w-full">
                     <a class="@active('log.*', 'shadow-soft-xl rounded-lg bg-white font-semibold text-slate-700') py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors group" href="{{ route('log.index') }}">
                         <div class="@active('log.*', 'bg-orange') group-hover:bg-orange shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
@@ -170,6 +171,8 @@
                         <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Log</span>
                     </a>
                 </li>
+                @endif
+
 
                 <li class="mt-0.5 w-full">
                     <form action="{{ route('logout') }}" method="POST" class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors group">
