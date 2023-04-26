76d7bfb367e66193bc339649c1c499ee5550ef8d
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-04-26 08:46:22 +0700

    Reservation migration

---
+++ database/migrations/2023_03_31_070223_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('guest_id')->constrained('guests');
+            $table->foreignId('room_id')->constrained('rooms');
+            $table->foreignId('facility_id')->constrained('facilities');
+            $table->datetime('check_in');
+            $table->datetime('check_out');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
