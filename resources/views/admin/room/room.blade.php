3a61f9ced0222baac1b3017230842989308f6fe7
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-04-27 13:09:17 +0700

    null safe pointer (room page)

--- resources/views/admin/room/room.blade.php
+++ resources/views/admin/room/room.blade.php
@@ -61,7 +61,7 @@
                                             {{ $room->room_number }}
                                         </td>
                                         <td class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
-                                            {{ $room->facility->facility_name }}
+                                            {{ $room->facility->facility_name ?? '' }}
                                         </td>
                                         <td class="px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70">
                                             {{ $room->room_price }}
